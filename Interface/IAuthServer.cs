173b0731fb343e4553098eb6d9ec2cf64db16c48
Xisomiddin <xerox2000gofuorv@gmail.com>
2023-01-25 10:28:16 +0500

    I Append Auth

---
+++ Interface/IAuthServer.cs
@@ -0,0 +1,10 @@
+﻿using NajotEdu.ru.Registar;
+
+namespace NajotEdu.ru.Interface
+{
+    public interface IAuthServer
+    {
+        Task<string> Login(LoginRegistr request);
+        Task<string> Password(PasswordRegistr request);
+    }
+}
---
+++ Servis/AuthServer.cs
@@ -0,0 +1,18 @@
+﻿using NajotEdu.ru.Interface;
+using NajotEdu.ru.Registar;
+
+namespace NajotEdu.ru.Servis
+{
+    public class AuthServer : IAuthServer
+    {
+        public Task<string> Login(LoginRegistr request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> Password(PasswordRegistr request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
