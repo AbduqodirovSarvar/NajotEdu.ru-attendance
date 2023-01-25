e919f5f34e5c01698b91db3f3884df0e909c1473
sarvarbek <abduqodirovsarvar.2002@gmail.com>
2023-01-25 10:32:20 +0500

    Add Repositories

---
+++ Najot.data/IRepositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using NajotEdu.ru.Najot.Domain.AbstractEntities;
+using System.Linq.Expressions;
+
+namespace NajotEdu.ru.Najot.data.IRepositories
+{
+    public interface IGenericRepository<T> where T : Auditable
+    {
+        Task<T> CreateAsync(T entity);
+        Task<T> UpdateAsync(int id, T entity);
+        Task<T> DeleteAsync(int id);
+        Task<T> GetAsync(int id);
+        Task<T> GetAllAsync(Expression<Func<T, bool>> predicate);
+    }
+}
---
+++ Najot.data/Repositories/GenericRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NajotEdu.ru.Najot.data.DbContexts;
+using NajotEdu.ru.Najot.data.IRepositories;
+using NajotEdu.ru.Najot.Domain.AbstractEntities;
+using System.Linq.Expressions;
+
+namespace NajotEdu.ru.Najot.data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : Auditable
+    {
+        protected readonly AppDbContext _db;
+        protected readonly DbSet<T> _dbSet;
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAllAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(int id, T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
